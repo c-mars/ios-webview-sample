fa31a823f199bf3f2d5c056cd9c85c1c5a897477
Constantine Mars <constantine.mars@gmail.com>
2016-03-15 13:00:19 +0200

    Initial Commit

---
+++ WebViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewSample
+//
+//  Created by Constantine Mars on 3/15/16.
+//  Copyright © 2016 Constantine Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebViewSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebViewSample
+//
+//  Created by Constantine Mars on 3/15/16.
+//  Copyright © 2016 Constantine Mars. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
